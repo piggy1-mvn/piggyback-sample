8500dd50d80e814ef314025769ef70478647d9f4
Rajan Gupta <rajan0493@gmail.com>
2019-09-27 20:28:44 +0800

    Update Redirect URL

--- src/main/java/com/incentives/piggyback/sampleApp/constants/Constant.java
+++ src/main/java/com/incentives/piggyback/sampleApp/constants/Constant.java
@@ -8,7 +8,7 @@ public interface Constant {
 	
 	final String partnerDisplayName = "FOOD STORE";
 	
-	final String partnerRedirectUrl = "http://localhost:8080/";
+	final String partnerRedirectUrl = "http://108.59.87.44:8087/home";
 	
 	final String partnerId = "924cbcf2-51c1-44da-8d9c-92e0b7e570cf";
 	
