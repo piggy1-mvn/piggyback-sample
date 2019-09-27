9f26d0efccafc6dfa951842bbbaeb6c44b16a3bc
Dipty Ojha <e0401948@u.nus.edu>
2019-09-27 18:17:38 +0800

    image build changes

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
-FROM openjdk:11
-VOLUME /tmp
-ARG DEPENDENCY=target/dependency
-COPY ${DEPENDENCY}/BOOT-INF/lib /app/lib
-COPY ${DEPENDENCY}/META-INF /app/META-INF
-COPY ${DEPENDENCY}/BOOT-INF/classes /app
-ENTRYPOINT ["java","-cp","app:app/lib/*","com.incentives.piggyback.sampleApp.DemoApplication"]
+FROM java:8-jdk-alpine
+WORKDIR /app
+EXPOSE 8080
+ARG DEPENDENCY=target
+COPY ${DEPENDENCY}/demoApplication-0.0.1-SNAPSHOT.war /app
+ENTRYPOINT ["java","-jar","demoApplication-0.0.1-SNAPSHOT.war"]
+
+
--- src/test/java/com/incentives/piggyback/sampleApp/DemoApplicationTests.java
+++ src/test/java/com/incentives/piggyback/sampleApp/DemoApplicationTests.java
@@ -2,15 +2,26 @@ package com.incentives.piggyback.sampleApp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.powermock.modules.junit4.PowerMockRunner;
 
-@RunWith(SpringRunner.class)
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.verifyStatic;
+
+@RunWith(PowerMockRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
 	@Test
+	@PrepareForTest(SpringApplication.class)
 	public void contextLoads() {
+		mockStatic(SpringApplication.class);
+		DemoApplication.main(new String[]{"Hello", "World"});
+		verifyStatic(SpringApplication.class);
+		SpringApplication.run(DemoApplication.class, "Hello", "World");
+
 	}
 
 }
